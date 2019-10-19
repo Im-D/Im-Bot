b08762b8b647a5fbd30f71eed64a210395bd0173
BKJang <jp302119@gmail.com>
2019-10-19 21:28:37 +0900

    :package: reviewerList.json 경로 변경

--- packages/pr-reviewer/index.js
+++ packages/pr-reviewer/index.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const reviewerList = require('reviewerList.json')
+const reviewerList = require('./reviewerList.json')
 
 async function run() {
   try {
