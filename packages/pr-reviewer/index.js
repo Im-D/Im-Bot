ebf52ee602e27fe9eda28360ef8c3e97525edaf7
BKJang <jp302119@gmail.com>
2019-10-19 21:49:18 +0900

    :package: console owner parameter

--- packages/pr-reviewer/index.js
+++ packages/pr-reviewer/index.js
@@ -7,12 +7,16 @@ async function run() {
     const myToken = core.getInput('myToken');
     const octokit = new github.GitHub(myToken);
     const { owner, repo, number } = github.context.issue
+    console.log(owner)
 
     if(!number) {
       console.warn("Dont have number")
       return 
     }
     
+    // const reviewers = reviewerList.reviewers && reviewerList.reviewerList.filter(reviewer => {
+    //   return 
+    // })
     octokit.pulls.createReviewRequest({ owner, repo, pull_number:number, reviewers: reviewerList.reviewers })
   }
   catch (error) {
