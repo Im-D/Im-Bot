598c3800980f6a5c9e49407e7b25d629396cabb5
BKJang <jp302119@gmail.com>
2019-10-19 21:04:55 +0900

    📦 pr-reviewer index.js for testing

--- packages/pr-reviewer/index.js
+++ packages/pr-reviewer/index.js
@@ -1,11 +1,20 @@
 const core = require('@actions/core');
+const github = require('@actions/github');
+const reviewerList = require('reviewerList.json')
 
 async function run() {
-  try { 
-    core.debug((new Date()).toTimeString())
-    core.debug((new Date()).toTimeString())
-    core.setOutput('time', new Date().toTimeString());
-  } 
+  try {
+    const myToken = core.getInput('myToken');
+    const octokit = new github.GitHub(myToken);
+    const { owner, repo, number } = github.context.issue
+
+    if(!number) {
+      console.warn("Dont have number")
+      return 
+    }
+    
+    octokit.pulls.createReviewRequest({ owner, repo, pull_number:number, reviewers: reviewerList.reviewers })
+  }
   catch (error) {
     core.setFailed(error.message);
   }
