945eee5981a1e342d6fa6e38faca52ed02ed4140
SeonHyungJo <seonhyung.jo@gmail.com>
2019-10-22 15:20:35 +0900

    :package: SeonHyungJo / Create update-readme action

--- packages/update-readme/index.js
+++ packages/update-readme/index.js
@@ -10,13 +10,12 @@ async function run() {
 
     const path = 'README.md'
 
-    const contents = await octokit.repos.getContents({
+    const readme = await octokit.repos.getReadme({
       owner,
-      repo,
-      path,
+      repo
     })
 
-    const buff = Buffer.from(contents.data.content, 'base64').toString('utf8');
+    const buff = Buffer.from(readme.data.content, 'base64').toString('utf8');
 
     const updateReadme = async (owner, repo, path, message, content, sha) => await octokit.repos.createOrUpdateFile({
       owner,
@@ -26,11 +25,10 @@ async function run() {
       content,
       sha
     })
-    
-    await updateReadme(owner, repo, path, "update README.md", '', payload.pull_request.base.sha)
-    
-    console.log('payload__', payload.pull_request.base.sha)
+
+    console.log('payload__', readme)
     console.log('content__', buff)
+    await updateReadme(owner, repo, path, "update README.md", '', readme.data.sha)
   } 
   catch (error) {
     core.setFailed(error.message);
