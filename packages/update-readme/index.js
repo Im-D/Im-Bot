1a2eeac8963737f8dc633667a34631949e69059d
SeonHyungJo <seonhyung.jo@gmail.com>
2019-10-22 23:24:00 +0900

    :package: SeonHyungJo / 공통작업을 하면서 발생한 버그 수정

--- packages/update-readme/index.js
+++ packages/update-readme/index.js
@@ -12,12 +12,10 @@ async function run() {
     const linkLocTarget = core.getInput('linkLocTarget');
 
     const octokit = new github.GitHub(myToken);
-    const {pull_request, sender} = github.context.payload
+    const { pull_request, sender } = github.context.payload
     const { owner, repo, number } = github.context.issue
-    const readmeData = await getReadme( octokit, owner, repo )
+    const { data: { content, sha } } = await getReadme(octokit, owner, repo)
 
-    console.log('readmeData___', readmeData)
-    
     // Create Updated(Create) Markdown files
     const fileList = await updatedFilelist(octokit, owner, repo, number)
     const fileLinkContent = fileList.data.reduce((acc, cur) => {
@@ -28,14 +26,14 @@ async function run() {
       return acc
     }, '')
 
-    const originalContent = Buffer.from(readmeData.data.content, 'base64').toString('utf8');
+    const originalContent = Buffer.from(content, 'base64').toString('utf8');
     const splitContent = originalContent.split(linkLocTarget)
-    
+
     // Create New README Content
     const newContent = Buffer.from(splitContent[0] + linkLocTarget + '\n' + fileLinkContent + splitContent[1], 'utf8').toString('base64');
 
     // Update README
-    await updateReadme(octokit, owner, repo, README_PATH, "update README.md", newContent, readmeData.data.sha)
+    await updateReadme(octokit, owner, repo, README_PATH, "update README.md", newContent, sha)
   }
   catch (error) {
     core.setFailed(error.message);
