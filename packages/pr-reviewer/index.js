f9bac1e9625476b8e1528e24ac43ee6f2c910363
JangBongKi <jp302119@gmail.com>
2020-05-20 13:50:11 +0900

    📦Update setting for get random reviewers

--- packages/pr-reviewer/index.js
+++ packages/pr-reviewer/index.js
@@ -3,10 +3,28 @@ const github = require('@actions/github');
 const reviewerList = require('./reviewerList.json')
 const { createReviewRequest } = require('../common/octokit')
 
+const REVIEWER_COUNT = 4
+
+function getRandomItem(arr) {
+  return arr[Math.floor(Math.random() * arr.length)]
+}
+
+function getReviewerList(arr) {
+  let resultArr = []
+  while(resultArr.length < REVIEWER_COUNT) {
+    const randomReviewer = getRandomItem(arr)
+    if (!resultArr.includes(randomReviewer)) {
+      resultArr.push(randomReviewer)
+    }
+  }
+
+  return resultArr
+}
+
 async function run() {
   try {
-    const myToken = core.getInput('myToken');
-    const octokit = new github.GitHub(myToken);
+    const myToken = core.getInput('myToken')
+    const octokit = new github.GitHub(myToken)
     const author  = github.context.payload.pull_request.user.login
     const { owner, repo, number } = github.context.issue
 
@@ -19,7 +37,9 @@ async function run() {
       return reviewer !== author
     })
 
-    createReviewRequest(octokit, owner, repo, number, reviewers)
+    const filteredReviewers = getReviewerList(reviewers)
+
+    createReviewRequest(octokit, owner, repo, number, filteredReviewers)
   }
   catch (error) {
     core.setFailed(error.message);
