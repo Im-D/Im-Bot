a9c54b5b59ab303c5696a0bb82e13de8b3d8e168
JangBongKi <jp302119@gmail.com>
2020-07-27 15:57:43 +0900

    📦 Update util.js

--- packages/common/util.js
+++ packages/common/util.js
@@ -1,6 +1,6 @@
 const path = require('path')
 
-const createFilelink = (headData, branch, fileName) => headData.repo.html_url + path.join('/blob', branch, fileName)
+const createFilelink = (headData, branch, fileName) => encodeURI(headData.repo.html_url + path.join('/blob', branch, fileName))
 
 module.exports = {
   createFilelink
